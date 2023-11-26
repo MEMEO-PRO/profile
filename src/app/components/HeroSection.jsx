91e8acb3d86885efe8365e97e9ac0252cdc2bad7
MEMEO-PRO <yash.ecs@gmail.com>
2023-11-26 06:48:10 +0530

    Add links

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
-
+import Link from "next/link";
 const HeroSection = () => {
   return (
     <section className="grid grid-cols-1 lg:grid-cols-12 my-4">
@@ -35,11 +35,13 @@ const HeroSection = () => {
           <button className="bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white px-6 py-3 rounded-full mr-4">
             Hire Me
           </button>
-          <button className="m-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 px-1 py-1  text-white rounded-full">
-            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-              Download CV
-            </span>
-          </button>
+          <Link href="https://drive.google.com/file/d/1Bvuz3dVvJ6zqOfJ_bRfKH9C6VIC7habv/view?usp=sharing">
+            <button className="m-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 px-1 py-1  text-white rounded-full">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
       <div className="col-span-5 place-self-center mt-4 lg:mt-0">
