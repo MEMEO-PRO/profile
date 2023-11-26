ffe80fb9869f198b72469bec4f2dd6c9485c1b89
MEMEO-PRO <yash.ecs@gmail.com>
2023-11-26 06:49:47 +0530

    Add links

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,20 +1,20 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
+// // import { EmailTemplate } from '../../../components/EmailTemplate';
+// import { NextResponse } from 'next/server';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: 'Yash <sunnyeng1981@gmail.com>',
-      to: ['yash.ecs@gmail.com'],
-      subject: 'Hello world',
-      react: <><p>Emailbdy</p></>
-    });
+// export async function POST() {
+//   try {
+//     const data = await resend.emails.send({
+//       from: 'Yash <sunnyeng1981@gmail.com>',
+//       to: ['yash.ecs@gmail.com'],
+//       subject: 'Hello world',
+//       react: <><p>Emailbdy</p></>
+//     });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
